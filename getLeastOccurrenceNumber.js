4e8a1ba610b8c497b030117a675da30762312812
Isabella Dang <isabelladang173@gmail.com>
2020-06-16 12:28:37 +0200

    Add files via upload

---
+++ getLeastOccurrenceNumber.js
@@ -0,0 +1,19 @@
+//Write a JavaScript function that accepts an array of numbers and return the number which has the smallest occurrence among the array elements
+// input [ 2, 4, 3, 4, 3, 8, 8] - > output "2"
+// input [ 2, 4, 3, 4, 3, 2, 8] - > outtput "8"
+function getLeastOccurrentNumber(arr) {
+    //For loop that count the appearance of every number in the array
+    const frequencyCounter = {}
+    for (let val of arr) {
+        frequencyCounter[val] = (frequencyCounter[val] || 0) + 1
+    }
+    //Convert the object into array
+    const countedArray = Object.entries(frequencyCounter)
+    //Sort the counted array from the least to the most appearance number
+    countedArray.sort(function(a, b) {
+        return a[1] - b[1];
+    })
+    //Return the least occurance number
+    return countedArray[0][0]
+}
+console.log(getLeastOccurrentNumber([2, 4, 3, 4, 3, 8, 8]))
